132f231cd06dcf25530a4d1578d6da379d7b5cb7
Jaimie <jaimie.j.hemmings@gmail.com>
2024-10-05 15:46:33 +0100

    add: additional ui tests for fundamentals page

--- cypress/e2e/fundamentals.cy.js
+++ cypress/e2e/fundamentals.cy.js
@@ -1,5 +1,23 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Fundamentals test', () => {
+  beforeEach(() => {
+    cy.visit('/fundamentals')
+  })
+
+  it('has valid header text', () => {
+    cy.getDataTest('fundamental-header')
+      .should('contain.text', 'Testing Fundamentals')
+      .should('not.be.empty')
+      .should('have.length', 1)
+      .invoke('text').then((text) => {
+        expect(text.length).to.be.greaterThan(1)
+      })
+  })
+
+  it('The accordion works correctly', () => {
+    cy.contains(/Your tests will exist in a describe block/i).should('not.be.visible')
+    cy.get('[data-test="accordion-item-1"] div[role="button"]').click()
+    cy.contains(/Your tests will exist in a describe block/i).should('be.visible')
+    cy.get('[data-test="accordion-item-1"] div[role="button"]').click()
+    cy.contains(/Your tests will exist in a describe block/i).should('not.be.visible')
   })
 })
